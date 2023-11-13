a3f2ed920e0ddc7b9a1c6c8928d9c1208e2f3f9b
Kevin Salazar <80077213+Kevin-Salazar-itcr@users.noreply.github.com>
2023-11-12 20:57:24 -0600

    Agenda en interfaz

---
+++ vs_project/duendeMakeApp/Controllers/AgendasController.cs
@@ -0,0 +1,32 @@
+﻿using duendeMakeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace duendeMakeApp.Controllers
+{
+    public class AgendasController : Controller
+    {
+        private readonly ILogger<AgendasController> _logger;
+
+        public AgendasController(ILogger<AgendasController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
